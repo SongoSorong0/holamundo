dcc854c7ee58ef057dd9e2c525a98016d4397a62
SongoSorong0 <sacastillo70@misena.edu.co>
2021-05-29 16:21:42 -0500

    Remover librerias no usadas

--- src/App.js
+++ src/App.js
@@ -1,26 +1,10 @@
 import "./App.css";
 import DataTable from "./components/DataTable.js";
-import { dataSource2 } from "./data/dataSource2.js";
 import {dataSource3}  from "./data/dataSource.json";
-import React, { useEffect } from "react";
+
 
 function App() {
-  const [items,setItems] = React.useState([]);
 
-  useEffect(()=>{
-    fetch("https://jsonplaceholder.typicode.com/todos")
-    .then(response => response.json())
-    .then(
-      (resu1) =>{
-        console.log("Resultado : ");
-        
-        console.log(JSON.stringify(resu1));
-      },
-      (error) =>{
-        console.log("Esto es un error");
-      }
-    )
-},[])
 
   const productos = [
     { serial: "101010", nombre: "lapicero", cantidad: "506" },
