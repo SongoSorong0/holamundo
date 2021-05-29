cf0c1274769ed58ea7102487b07997c1c62920f3
SongoSorong0 <sacastillo70@misena.edu.co>
2021-05-29 16:28:22 -0500

    Se remueve constante 'productos'

--- src/App.js
+++ src/App.js
@@ -6,14 +6,7 @@ import {dataSource3}  from "./data/dataSource.json";
 function App() {
 
 
-  const productos = [
-    { serial: "101010", nombre: "lapicero", cantidad: "506" },
-    { serial: "101010", nombre: "Cuaderno", cantidad: "69" },
-    { serial: "101010", nombre: "Resma de papel", cantidad: "777" },
-    { serial: "101010", nombre: "Borrador", cantidad: "123" },
-    { serial: "101010", nombre: "lapiz", cantidad: "9" },
-    { serial: "101010", nombre: "impresora", cantidad: "9" },
-  ];
+
 
   return (
     <div className="App">
